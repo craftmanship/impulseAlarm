17b69902c79a7df99222588fef3aac353f6426c0
matt <craftmanshi9@gmail.com>
2018-10-05 17:39:42 +0900

    Initial commit 181005

---
+++ impulseAlarm/impulseAlarm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  impulseAlarm
+//
+//  Created by 406511 on 05/10/2018.
+//  Copyright © 2018 406511. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ impulseAlarm/impulseAlarmTests/impulseAlarmTests.swift
@@ -0,0 +1,34 @@
+//
+//  impulseAlarmTests.swift
+//  impulseAlarmTests
+//
+//  Created by 406511 on 05/10/2018.
+//  Copyright © 2018 406511. All rights reserved.
+//
+
+import XCTest
+@testable import impulseAlarm
+
+class impulseAlarmTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
