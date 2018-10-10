dc2239ac16febe685d6baf79ce700e3b17f4c88c
Matt <craftmanshi9@gmail.com>
2018-10-10 17:19:05 +0900

    ViewController 주석 추가

--- impulseAlarm/impulseAlarm/ViewController.swift
+++ impulseAlarm/impulseAlarm/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // 알람 동작
+        
+        
+        // 리스트뷰
+        
+        
+        // 해당기간 도달시 로컬푸시알람
+        
+        
+        
+        // 알람등록
+        // 알람은 주기를 설정하는 방식으로 동작한다
+        
+        
+        // ex) 치킨, 짜장면, 옷지름, 목욕, 손톱깎기, 머리자르러가기, 칫솔 사용기간
     }
 
 
